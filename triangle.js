f8d1a335d8b0cbdde9ff4720d6607e427f1b4407
Naoki Shimizu <xnaok@Naoki-MacBook-Pro.local>
2022-02-24 23:56:10 +0900

    Fixed files

---
+++ triangle.js
@@ -0,0 +1,11 @@
+const baseValue = prompt('Enter the base of a triangle: ');
+const heightValue = prompt('Enter the height of a triangle: ');
+
+// calculate the areaa
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+    `The area of the triangle is ${areaValue}`
+);
+
+// References: https://www.programiz.com/javascript/examples/area-triangle
