f1e3e43abbcbc8cd28b49ad5424d000244c3d67e
Naoki Shimizu <xnaok@Naoki-MacBook-Pro.local>
2022-02-25 23:53:56 +0900

    Fixed comments in files

--- triangle.js
+++ triangle.js
@@ -1,7 +1,7 @@
 const baseValue = prompt('Enter the base of a triangle: ');
 const heightValue = prompt('Enter the height of a triangle: ');
 
-// calculate the areaa
+// calculate the area
 const areaValue = (baseValue * heightValue) / 2;
 
 console.log(
